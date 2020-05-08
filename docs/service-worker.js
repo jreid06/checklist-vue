1d78283db7fca57a13939694730524f5b04c8c83
Jason Reid <jason.reid@appraisd.com>
2020-05-08 01:53:37 +0100

    app build - 8th may

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84c9c701e3491d4ea0c772d314d4e097.js"
+  "/precache-manifest.66cf256bced3d63f20345405d9010e67.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "checklist-calendar-app"});
